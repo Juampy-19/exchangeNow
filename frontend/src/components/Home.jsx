3e05d7b39518d833eb92025f3a89b5d56418ee2d
Juampy-19 <jpcastellano1990@gmail.com>
2025-04-23 14:55:56 -0300

    Corrección de estilos en el componente Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from './Button';
 const Home = () => {
     return (
-        <div className='h-[80%] flex flex-col items-center justify-center gap-20'>
+        <div className='h-[35em] md:h-[25em] flex flex-col items-center justify-center gap-20'>
             <Button to='/crypto'>Criptomonedas</Button>
             <Button to='/fiat'>Monedas Oficiales</Button>
         </div>
